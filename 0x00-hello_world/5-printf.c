99d9b68c8e1b17b2984f2599908d9a2fed96a429
mera1642002 <amirasahwan0@gmail.com>
2023-03-16 20:34:56 -0700

    my commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry point
- *
- *Description: A C program that prints with printf function.
- *
- * Return: Always 0 (success)
+* main - Entry
+* Return:0
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\);
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
-
